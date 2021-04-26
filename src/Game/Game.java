978dded639e2b7e0111429ad2f776ecfa793cbfb
Vallari Eastman <vallarieastman@gmail.com>
2021-04-25 18:32:09 -0700

    Update Game.java

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -28,7 +28,7 @@ public class Snake {
         tail.setCoordinateType(CoordinateType.EMPTY);
  
         head = nextCoordinate;
-        head.setCoordinateType(CellType.SNAKE);
+        head.setCoordinateType(CoordinateType.SNAKE);
         snakeBodyList.addFirst(head);
     }
     
@@ -155,6 +155,9 @@ public class Grid {
 
 //class game represents the game; stores info about the snake and the grid. 
 public class Game {
+    public static final int DIRECTION_NONE = 0, DIRECTION_RIGHT = 1,
+    DIRECTION_LEFT = -1, DIRECTION_UP = 2, DIRECTION_DOWN = -2;
+    
     private Snake snake;
     private Grid grid;
     private int direction;
@@ -167,47 +170,110 @@ public class Game {
     }
     
     //gets Snake
+    public Snake getSnake()
+    {
+        return snake;
+    }
     
     //sets Snake
+    public void setSnake(Snake snake)
+    {
+        this.snake = snake;
+    }
     
-    //gets Board
+    //gets Grid
+    public Grid getGrid()
+    {
+        return grid;
+    }
     
-    //sets Board
+    //sets Grid
+    public void setGrid(Grid grid)
+    {
+        this.grid = grid;
+    }
     
     //gets Direction
+    public int getDirection()
+    {
+        return direction;
+    }
     
     //sets Direction
+    public void setDirection(int direction)
+    {
+        this.direction = direction;
+    }
     
-    public void update(); //the grid will need updates regularly
+    //gets game over
+    public boolean isGameOver()
+    {
+        return gameOver;
+    }
+ 
+    //sets game over
+    public void setGameOver(boolean gameOver)
+    {
+        this.gameOver = gameOver;
+    }
+    
+    //the grid will need updates regularly
+    public void update();
     {
         if (!gameOver){
-        }
-        else {
+            if (direction != DIRECTION_NONE) {
+                Coordinate nextCoordinate = getNextCoordinate(snake.getHead());
+                if (snake.checkCrash(nextCoordinate)) {
+                    setDirection(DIRECTION_NONE);
+                    gameOver = true;
+                }
+                else {
+                    snake.move(nextCoordinate);
+                    if (nextCoordinate.getCoordinateType() == CoordinateType.FOOD) {
+                        snake.grow();
+                        grid.generateFood();
+                    }
+                }
+            }
         }
     }
     
     //gets next Coordinate
-    
+    private Coordinate getNextCoordinate(Coordinate currentPosition)
+    {
+        System.out.println("Going to find next cell");
+        int y = currentPosition.getY();
+        int x = currentPosition.getX();
+ 
+        if (direction == DIRECTION_RIGHT) {
+            x++;
+        }
+        else if (direction == DIRECTION_LEFT) {
+            x--;
+        }
+        else if (direction == DIRECTION_UP) {
+            y--;
+        }
+        else if (direction == DIRECTION_DOWN) {
+            y++;
+        }
+ 
+        Coordinate nextCoordinate = grid.getCoordinates()[y][x];
+ 
+        return nextCoordinate;
+    }
 
     public static class MainMenu {
-        private String startGame;
-        private String setting;
-        private String quitGame;
-        private boolean input;
-
-        public void startGame() {
-        }
-
-        public void quitGame() {
-        }
-
-        public void toSettings() {
-        }
-
-        public void input() {
-        }
-
-        public static class Settings {
+        System.out.println("Start Game");
+        
+        Coordinate initPos = new Coordinate(0, 0);
+        Snake initSnake = new Snake(initPos);
+        Grid grid = new grid(20, 20);
+        Game newGame = new Game(initSnake, grid);
+        newGame.gameOver = false;
+        newGame.direction = DIRECTION_RIGHT;
+        
+       /* public static class Settings {
             private String difficulty;
             private boolean returnToMenu;
             private boolean snakeSkin;
@@ -220,6 +286,7 @@ public class Game {
 
             public void changeDifficulty() {
             }
+            */
         }
     }
 }
